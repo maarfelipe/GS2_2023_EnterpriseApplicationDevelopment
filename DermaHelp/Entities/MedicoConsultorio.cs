09319b371974fb7f4cb2c05b6b4723a44a0e3df7
Matheus Fellipe_dell <matheus.maarfe@hotmail.com>
2023-11-19 14:59:54 -0300

    feat: define entities, attributes, and relationships
    - Create entity classes (Usuario, Imagem, Consulta, Consultorio, Endereco, Medico, MedicoConsultorio) with their respective attributes.
    - Define relationships between entities using navigation properties and Fluent API in the 'OracleDbContext'.
    - Run 'add-migration' to generate the database migration.
    - Apply the migration using 'update-database' to synchronize the database schema.
    
    #BREAKING CHANGE: None

---
+++ DermaHelp/Entities/MedicoConsultorio.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DermaHelp.Entities
+{
+    [Table("MedicosConsultorios")]
+    public class MedicoConsultorio
+    {
+        [Column("id_medico")]
+        public long? MedicoId { get; set; }
+        public virtual Medico? Medico { get; set; }
+
+        [Column("id_consultorio")]
+        public long? ConsultorioId { get; set; }
+        public virtual Consultorio? Consultorio { get; set; }
+    }
+}
--- DermaHelp/Persistence/OracleDbContext.cs
+++ DermaHelp/Persistence/OracleDbContext.cs
@@ -5,8 +5,88 @@ namespace DermaHelp.Persistence
 {
     public class OracleDbContext : DbContext
     {
-        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Imagem> Imagem { get; set; }
+        public DbSet<Consulta> Consulta { get; set; }
+        public DbSet<Endereco> Endereco { get; set; }
+        public DbSet<Consultorio> Consultorio { get; set; }
+        public DbSet<Medico> Medico { get; set; }
+        public DbSet<MedicoConsultorio> MedicoConsultorio { get; set; }
 
         public OracleDbContext(DbContextOptions<OracleDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // 1:N Usuario-Imagem relationship
+            modelBuilder.Entity<Imagem>()
+                .HasKey(i => i.Id);
+
+            modelBuilder.Entity<Usuario>()
+                .HasMany(u => u.Imagens)
+                .WithOne(i => i.Usuario)
+                .HasForeignKey(i => i.UsuarioId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // 1:N Usuario-Consulta relationship
+            modelBuilder.Entity<Consulta>()
+                .HasKey(c => c.Id);
+
+            modelBuilder.Entity<Usuario>()
+                .HasMany(u => u.Consultas)
+                .WithOne(c => c.Usuario)
+                .HasForeignKey(c => c.UsuarioId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // 1:N Consultorio-Consulta relationship
+            modelBuilder.Entity<Consultorio>()
+                .HasKey(c => c.Id);
+
+            modelBuilder.Entity<Consulta>()
+                .HasOne(c => c.Consultorio)
+                .WithMany(c => c.Consultas)
+                .HasForeignKey(c => c.ConsultorioId)
+                .IsRequired(true) // Assuming Consulta must have a Consultorio
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // 1:1 Consultorio-Endereco relationship  
+            modelBuilder.Entity<Consultorio>()
+                .HasKey(c => c.Id);
+
+            modelBuilder.Entity<Consultorio>()
+                .HasOne(c => c.Endereco)
+                .WithOne(e => e.Consultorio)
+                .HasForeignKey<Endereco>(e => e.ConsultorioId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // 1:1 Consulta-Medico relationship  
+            modelBuilder.Entity<Medico>()
+                .HasKey(m => m.Id);
+
+            modelBuilder.Entity<Consulta>()
+                .HasOne(c => c.Medico)
+                .WithMany(m => m.Consultas)
+                .HasForeignKey(c => c.MedicoId)
+                .IsRequired(true) // Assuming Consulta must have a Medico
+                .OnDelete(DeleteBehavior.Restrict); // This line specifies that deleting a Medico will not delete associated Consultas
+
+            // N:N Medico-Consultorio relationship
+            modelBuilder.Entity<MedicoConsultorio>()
+                .HasKey(mc => new { mc.MedicoId, mc.ConsultorioId });
+
+            modelBuilder.Entity<MedicoConsultorio>()
+                .HasOne(mc => mc.Medico)
+                .WithMany(m => m.MedicoConsultorio)
+                .HasForeignKey(mc => mc.MedicoId)
+                .IsRequired(false);
+
+            modelBuilder.Entity<MedicoConsultorio>()
+                .HasOne(mc => mc.Consultorio)
+                .WithMany(c => c.MedicoConsultorio)
+                .HasForeignKey(mc => mc.ConsultorioId)
+                .IsRequired(false);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
