013e2bf31bcce47d22276ff98c6862956c77fb08
Matheus Fellipe_dell <matheus.maarfe@hotmail.com>
2023-11-19 10:45:30 -0300

    feat: set up ASP.NET Core MVC project with Oracle DB and .NET 7.0
    - Initialize a new ASP.NET Core MVC project using .NET 7.0.
    - Configure the connection to the Oracle database in the 'OracleDbContext'.
    - Install the following dependencies:
      - Microsoft.EntityFrameworkCore 7.0.14
      - Microsoft.EntityFrameworkCore.Tools 7.0.14
      - Microsoft.VisualStudio.Web.CodeGeneration.Design 7.0.11
      - Oracle.EntityFrameworkCore 7.21.12
    
    #BREAKING CHANGE: None

---
+++ DermaHelp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DermaHelp.Entities
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ DermaHelp/Persistence/OracleDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DermaHelp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DermaHelp.Persistence
+{
+    public class OracleDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public OracleDbContext(DbContextOptions<OracleDbContext> options) : base(options) { }
+    }
+}
