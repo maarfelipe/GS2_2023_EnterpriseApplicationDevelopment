8f4ae5529ee7510752d7ba9fde7049d4d0cfeda7
Matheus Fellipe_dell <matheus.maarfe@hotmail.com>
2023-11-21 23:11:39 -0300

    feat: add Usuario controller
    - Generate a controller for the Usuario entity using Visual Studio controller generator.
    - Include CRUD actions and views for managing Usuario entities.
    
    #BREAKING CHANGE: None

--- DermaHelp/Controllers/UsuarioController.cs
+++ DermaHelp/Controllers/UsuarioController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using DermaHelp.Entities;
+﻿using DermaHelp.Entities;
 using DermaHelp.Persistense;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DermaHelp.Controllers
 {
