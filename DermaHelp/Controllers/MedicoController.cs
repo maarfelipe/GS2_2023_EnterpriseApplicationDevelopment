731eb22557a8a1c4e34f0beff79fa4da674aa130
Matheus Fellipe_dell <matheus.maarfe@hotmail.com>
2023-11-21 23:14:10 -0300

    feat: add Medico controller
    - Generate a controller for the Medico entity using Visual Studio controller generator.
    - Include CRUD actions and views for managing Medico entities.
    
    #BREAKING CHANGE: None

---
+++ DermaHelp/Controllers/MedicoController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DermaHelp.Entities;
+using DermaHelp.Persistense;
+
+namespace DermaHelp.Controllers
+{
+    public class MedicoController : Controller
+    {
+        private readonly PostgreDbContext _context;
+
+        public MedicoController(PostgreDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Medico
+        public async Task<IActionResult> Index()
+        {
+            var postgreDbContext = _context.Medico.Include(m => m.Consultorio);
+            return View(await postgreDbContext.ToListAsync());
+        }
+
+        // GET: Medico/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Medico == null)
+            {
+                return NotFound();
+            }
+
+            var medico = await _context.Medico
+                .Include(m => m.Consultorio)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (medico == null)
+            {
+                return NotFound();
+            }
+
+            return View(medico);
+        }
+
+        // GET: Medico/Create
+        public IActionResult Create()
+        {
+            ViewData["ConsultorioId"] = new SelectList(_context.Consultorio, "Id", "Cnpj");
+            return View();
+        }
+
+        // POST: Medico/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Crm,Email,ConsultorioId")] Medico medico)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(medico);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ConsultorioId"] = new SelectList(_context.Consultorio, "Id", "Cnpj", medico.ConsultorioId);
+            return View(medico);
+        }
+
+        // GET: Medico/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Medico == null)
+            {
+                return NotFound();
+            }
+
+            var medico = await _context.Medico.FindAsync(id);
+            if (medico == null)
+            {
+                return NotFound();
+            }
+            ViewData["ConsultorioId"] = new SelectList(_context.Consultorio, "Id", "Cnpj", medico.ConsultorioId);
+            return View(medico);
+        }
+
+        // POST: Medico/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Nome,Crm,Email,ConsultorioId")] Medico medico)
+        {
+            if (id != medico.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(medico);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MedicoExists(medico.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ConsultorioId"] = new SelectList(_context.Consultorio, "Id", "Cnpj", medico.ConsultorioId);
+            return View(medico);
+        }
+
+        // GET: Medico/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Medico == null)
+            {
+                return NotFound();
+            }
+
+            var medico = await _context.Medico
+                .Include(m => m.Consultorio)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (medico == null)
+            {
+                return NotFound();
+            }
+
+            return View(medico);
+        }
+
+        // POST: Medico/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Medico == null)
+            {
+                return Problem("Entity set 'PostgreDbContext.Medico'  is null.");
+            }
+            var medico = await _context.Medico.FindAsync(id);
+            if (medico != null)
+            {
+                _context.Medico.Remove(medico);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MedicoExists(long id)
+        {
+          return (_context.Medico?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
