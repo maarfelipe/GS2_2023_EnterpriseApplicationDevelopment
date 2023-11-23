53093860016ce898f01854fccfcd875949e5ac53
Matheus Fellipe_dell <matheus.maarfe@hotmail.com>
2023-11-22 23:05:58 -0300

    refactor: refine Imagem entity and controller
    - Update Imagem entity attributes and validation for improved clarity.
    - Fine-tune Imagem controller actions and views for better functionality and user experience.
    
    #BREAKING CHANGE: None

---
+++ DermaHelp/Controllers/ImagemController.cs
@@ -0,0 +1,161 @@
+﻿using DermaHelp.Entities;
+using DermaHelp.Persistense;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace DermaHelp.Controllers
+{
+    public class ImagemController : Controller
+    {
+        private readonly PostgreDbContext _context;
+
+        public ImagemController(PostgreDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Imagem
+        public async Task<IActionResult> Index()
+        {
+            var postgreDbContext = _context.Imagem.Include(i => i.Usuario);
+            return View(await postgreDbContext.ToListAsync());
+        }
+
+        // GET: Imagem/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Imagem == null)
+            {
+                return NotFound();
+            }
+
+            var imagem = await _context.Imagem
+                .Include(i => i.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (imagem == null)
+            {
+                return NotFound();
+            }
+
+            return View(imagem);
+        }
+
+        // GET: Imagem/Create
+        public IActionResult Create()
+        {
+            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "Id", "Id");
+            return View();
+        }
+
+        // POST: Imagem/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,DataHora,ImageData,Resultado,UsuarioId")] Imagem imagem)
+        {
+            imagem.DataHora = DateTime.UtcNow;
+            _context.Add(imagem);
+            await _context.SaveChangesAsync();
+            TempData["SuccessMessage"] = $"Imagem {imagem.Id} cadastrada com sucesso.";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Imagem/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Imagem == null)
+            {
+                return NotFound();
+            }
+
+            var imagem = await _context.Imagem.FindAsync(id);
+            if (imagem == null)
+            {
+                return NotFound();
+            }
+            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "Id", "Id", imagem.UsuarioId);
+            return View(imagem);
+        }
+
+        // POST: Imagem/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,DataHora,ImageData,Resultado,UsuarioId")] Imagem imagem)
+        {
+            if (id != imagem.Id)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Update(imagem);
+                TempData["SuccessMessage"] = $"Imagem {imagem.Id} atualizada com sucesso.";
+
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ImagemExists(imagem.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Imagem/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Imagem == null)
+            {
+                return NotFound();
+            }
+
+            var imagem = await _context.Imagem
+                .Include(i => i.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (imagem == null)
+            {
+                return NotFound();
+            }
+
+            return View(imagem);
+        }
+
+        // POST: Imagem/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Imagem == null)
+            {
+                return Problem("Entity set 'PostgreDbContext.Imagem'  is null.");
+            }
+            var imagem = await _context.Imagem.FindAsync(id);
+            if (imagem != null)
+            {
+                _context.Imagem.Remove(imagem);
+            }
+            
+            await _context.SaveChangesAsync();
+            TempData["SuccessMessage"] = $"Imagem excluída com sucesso.";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ImagemExists(long id)
+        {
+          return (_context.Imagem?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- DermaHelp/Entities/Imagem.cs
+++ DermaHelp/Entities/Imagem.cs
@@ -1,31 +1,30 @@
-﻿//using System.ComponentModel.DataAnnotations.Schema;
-//using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
-//namespace DermaHelp.Entities
-//{
-//    [Table("Imagens")]
-//    public class Imagem
-//    {
-//        [Key]
-//        [Column("id")]
-//        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-//        public long Id { get; set; }
+namespace DermaHelp.Entities
+{
+    [Table("Imagens")]
+    public class Imagem
+    {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
 
-//        [Column("data_hora")]
-//        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-//        public DateTime DataHora { get; set; }
+        [Column("data_hora")]
+        public DateTime DataHora { get; set; }
 
-//        [Column("image_data")]
-//        [MaxLength]
-//        public required byte[] ImageData { get; set; }
+        [Column("image_data")]
+        [MaxLength(500)]
+        public required string ImageData { get; set; }
 
-//        [Column("resultado_imagem")]
-//        [MaxLength(500)]
-//        public string? Resultado { get; set; }
+        [Column("resultado_imagem")]
+        [MaxLength(500)]
+        public string? Resultado { get; set; }
 
-//        [ForeignKey("Usuario")]
-//        [Column("id_usuario")]
-//        public int UsuarioId { get; set; }
-//        public virtual required Usuario Usuario { get; set; }
-//    }
-//}
+        [ForeignKey("Usuario")]
+        [Column("id_usuario")]
+        public int UsuarioId { get; set; }
+        public virtual required Usuario Usuario { get; set; }
+    }
+}
